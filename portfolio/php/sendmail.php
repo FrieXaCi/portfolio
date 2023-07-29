ce48bbb5482805a661d045e2179403e34111ee42
Frieda Haringsma <fharingsma@hotmail.com>
2023-07-29 22:35:48 +0200

    send mail af met script bescherming

--- portfolio/php/sendmail.php
+++ portfolio/php/sendmail.php
@@ -1,18 +1,21 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $naam = $_POST["naam"];
-    $email = $_POST["email"];
-    $onderwerp = $_POST["onderwerp"];
-    $bericht = $_POST["bericht"];
-    
-    // Verzend e-mail met ingevulde gegevens
-    $to = "f.haringsma@xaci-development.com";
-    $subject = "Contactformulier ingevuld";
-    $message = "Naam: " . $naam . "\n";
-    $message .= "E-mail: " . $email . "\n";
-    $message .= "Onderwerp: " . $onderwerp . "\n";
-    $message .= "Bericht: " . $bericht . "\n";
-    mail($to, $subject, $message);
-}
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $naam = filter_input(INPUT_POST, "naam", FILTER_SANITIZE_SPECIAL_CHARS);
+        $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_SPECIAL_CHARS);
+        $onderwerp = filter_input(INPUT_POST, "onderwerp", FILTER_SANITIZE_SPECIAL_CHARS);
+        $bericht = filter_input(INPUT_POST, "bericht", FILTER_SANITIZE_SPECIAL_CHARS);
+        
+        // Verzend e-mail met ingevulde gegevens
+        $to = "f.haringsma@xaci-development.com";
+        $subject = "Contactformulier ingevuld";
+        $message = "Naam: " . $naam . "\n";
+        $message .= "E-mail: " . $email . "\n";
+        $message .= "Onderwerp: " . $onderwerp . "\n";
+        $message .= "Bericht: " . $bericht . "\n";
+        mail($to, $subject, $message);
+    }
+
+
 ?>
 
