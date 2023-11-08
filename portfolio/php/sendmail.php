cfbbd7bfdc3cd8789629ffc0c5069a23e4b83f5b
Frieda Haringsma <94936082+FrieXaCi@users.noreply.github.com>
2023-11-08 13:27:54 +0100

    whitespaces weggehaald in php script

--- portfolio/php/sendmail.php
+++ portfolio/php/sendmail.php
@@ -5,7 +5,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $onderwerp = filter_input(INPUT_POST, "onderwerp", FILTER_SANITIZE_SPECIAL_CHARS);
     $bericht = filter_input(INPUT_POST, "bericht", FILTER_SANITIZE_SPECIAL_CHARS);
 
-    // Verzend e-mail met ingevulde gegevens
     $to = "f.haringsma@xaci-development.com";
     $subject = "Contactformulier ingevuld";
     $message = "Naam: " . $naam . "\n";
@@ -14,19 +13,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message .= "Bericht: " . $bericht . "\n";
 
     if (mail($to, $subject, $message)) {
-        // Succesbericht
-        echo '<h1>Bedankt!</h1>';
+             echo '<h1>Bedankt!</h1>';
         echo '<p>Je bericht is succesvol verzonden.</p>';
         echo '<p>We nemen zo snel mogelijk contact met je op.</p>';
-        // Redirect naar de homepage na 3 seconden
-        header("refresh:3;url=https://www.xaci-development.com/");
+               header("refresh:3;url=https://www.xaci-development.com/");
         exit();
     } else {
-        // Foutmelding bij het verzenden van de e-mail
-        echo '<h1>Fout!</h1>';
+             echo '<h1>Fout!</h1>';
         echo '<p>Er is een fout opgetreden bij het verzenden van je bericht. Probeer het later opnieuw.</p>';
-        // Redirect naar de homepage na 3 seconden
-        header("refresh:3;url=https://www.xaci-development.com/");
+               header("refresh:3;url=https://www.xaci-development.com/");
         exit();
     }
 }
